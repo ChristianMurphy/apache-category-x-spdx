726ab8445f4e275a061b4b5c1e0c435afca14e38
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-09-26 21:10:36 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # apache-category-x-spdx
-Apache Category X SPDX codes
+
+[![NPM Version](https://img.shields.io/npm/v/apache-category-x-spdx.svg)](https://www.npmjs.com/package/apache-category-x-spdx)
+
+Apache Category X approved licenses in a machine readable format using [SPDX](https://spdx.org).
+
+Adapted from <https://apache.org/legal/resolved.html#what-can-we-not-include-in-an-asf-project-category-x>
+
+## Installation
+
+```sh
+# NPM
+npm install apache-category-x-spdx
+
+# Yarn
+yarn add apache-category-x-spdx
+```
+
+## Usage
+
+```js
+var list = require("apache-category-x-spdx");
+
+for (var i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = [
+  // Binary Code License (BCL)
+  "GPL-1.0-only",
+  "GPL-2.0-only",
+  "GPL-3.0-only",
+  "AGPL-3.0-only",
+  "LGPL-2.0-only",
+  "LGPL-2.1-only",
+  "LGPL-3.0-only",
+  "QPL-1.0",
+  "Sleepycat",
+  "CPOL-1.02",
+  "BSD-4-Clause",
+  "BSD-4-Clause-UC",
+  // JSR-275 License
+  // Microsoft Limited Public License
+  // Amazon Software License (ASL)
+  // Java SDK for Satori RTM license
+  "CC-BY-NC-1.0",
+  "CC-BY-NC-2.0",
+  "CC-BY-NC-2.5",
+  "CC-BY-NC-3.0",
+  "CC-BY-NC-4.0",
+  "CC-BY-NC-ND-1.0",
+  "CC-BY-NC-ND-2.0",
+  "CC-BY-NC-ND-2.5",
+  "CC-BY-NC-ND-3.0",
+  "CC-BY-NC-ND-4.0",
+  "CC-BY-NC-SA-1.0",
+  "CC-BY-NC-SA-2.0",
+  "CC-BY-NC-SA-2.5",
+  "CC-BY-NC-SA-3.0",
+  "CC-BY-NC-SA-4.0",
+  // Sun Community Source License 3.0
+  // Facebook BSD+Patents license
+  "NPL-1.0",
+  "NPL-1.1",
+  // The Solipsistic Eclipse Public License
+  // The "Don't Be A Dick" Public License
+  "JSON"
+];
